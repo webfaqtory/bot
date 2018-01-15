47c09dd7ab79de22cbdc88ca52f16707506b0fd2
John Burch <john@ideas.org>
2018-01-15 15:52:44 +0000

    first commit

--- index.js
+++ index.js
@@ -1,40 +1,31 @@
+const save = false;
+
+var websocket       = null;
 var users           = {};
 var con             = null;
-var websocket       = null;
 var currentPrice    = 0;
 var lastPrice       = 0;
 var price           = 0;
-var fixedArray      = require("fixed-array");
-var log             = fixedArray(2);
+var logLength       = 0;
+var margin          = 0;
+var log             = null;
 var mode            = "";
 var lastMode        = "";
+var key             = "";
+var secret          = "";
+var passphrase      = "";
+var wsURL           = "";
+var apiURL          = "";
+var product         = "";
+var currency        = "";
 
-const margin        = 2;
 const amount        = 0;
-const remote        = "www.johnburch.co.uk";
-const api           = "production";
+const remote        = process.argv[2];
 const Gdax          = require('gdax');
 const https         = require('https');
 const mysql         = require('mysql');
 const uuidv1        = require('uuid/v1');
-
-if (api == "production") {
-    var key           = '58d1428c5ccf65277a31c857d5279b62';
-    var secret        = 'q6jCM+x7NELTe2IVZXcEfqTPEv8fyL8Qs+Mo9OvpStWpkcpQ/Rt1XQ4uwDqKYNlbgPuWr/1phlIUDmrTCZpMJw==';
-    var passphrase    = 'krmk29adak';
-    var wsURL         = 'wss://ws-feed.gdax.com';
-    var apiURL        = 'https://api.gdax.com'
-    var product       = "ETH-EUR";
-    var currency      = "ETH";
-}else{
-    var key           = 'b20248335aa08863e7807457549ec245';
-    var secret        = 'FAFuC9FJxQsIIUx/6pGUjMLSZ3LoyN9zAZVd+PvzMrmL4UtqOtYLfdbQe6WYLgoZNaEKV5B69DjXp9fSFmk/Cw==';
-    var passphrase    = 'ub0prgu3xc9';
-    var wsURL         = 'wss://ws-feed-public.sandbox.gdax.com';
-    var apiURL        = 'https://api-public.sandbox.gdax.com'
-    var product       = "BTC-USD";
-    var currency      = "BTC";
-}
+const fixedArray    = require("fixed-array");
 
 // Setup MySQL
 var opt = {
@@ -59,38 +50,76 @@ var request = https.request(opt, function (res) {
         con.connect(function(err) {
             if (err) throw err;
         });
-
-        con.query("DELETE FROM bot", function (err, result) {
-            if (err) throw err;
-        });
-
-    });
-});
-request.end();
-
-// Get users
-opt = {
-    host: remote,
-    path: "/traders/" + currency + "?XDEBUG_SESSION_START=john",
-    port: 443,
-    method: 'GET'
-};
-request = https.request(opt, function (res) {
-    var data = '';
-    res.on('data', function (chunk) {
-        data += chunk;
-    });
-    res.on('end', function () {
-        users = JSON.parse(data);
-        for (var key in users) {
-            users[key].client = new Gdax.AuthenticatedClient(users[key].api_key, users[key].api_secret, users[key].passphrase, apiURL);
+        if (save) {
+            con.query("DELETE FROM bot", function (err, result) {
+                if (err) throw err;
+            });
         }
-        doDeal('buy', 980);
+        con.query("SELECT * FROM bot_config", function(err, rows, fields) {
+            if (err) throw err;
+            margin          = rows[0].margin;
+            logLength       = rows[0].points;
+            log             = fixedArray(logLength);
+            if (rows[0].api == "production") {
+                key           = rows[0].key;
+                secret        = rows[0].secret;
+                passphrase    = rows[0].passphrase;
+                wsURL         = rows[0].wsURL;
+                apiURL        = rows[0].apiURL;
+                product       = rows[0].product;
+                currency      = rows[0].currency;
+            }else{
+                key           = rows[0].sandbox_key;
+                secret        = rows[0].sandbox_secret;
+                passphrase    = rows[0].sandbox_passphrase;
+                wsURL         = rows[0].sandbox_wsURL;
+                apiURL        = rows[0].sandbox_apiURL;
+                product       = rows[0].sandbox_product;
+                currency      = rows[0].sandbox_currency;
+            }
+            // Now got all config
+            setTimeout(startup, 500);
+        });
+        con.query("SELECT price FROM bot_last", function(err, rows, fields) {
+            if (err) throw err;
+            price = rows[0].price;
+            if (price) {
+                for(var i = 0; i < logLength; i++) {
+                    log.push(price);
+                }
+            }
+        });
     });
 });
 request.end();
 
-websocketConnect();
+function startup() {
+    getUsers();
+    websocketConnect();
+}
+
+function getUsers() {
+    opt = {
+        host: remote,
+        path: "/traders/" + currency,
+        port: 443,
+        method: 'GET'
+    };
+    request = https.request(opt, function (res) {
+        var data = '';
+        res.on('data', function (chunk) {
+            data += chunk;
+        });
+        res.on('end', function () {
+            users = JSON.parse(data);
+            for (var key in users) {
+                users[key].client = new Gdax.AuthenticatedClient(users[key].api_key, users[key].api_secret, users[key].passphrase, apiURL);
+            }
+            doDeal('sell', price);
+        });
+    });
+    request.end();
+}
 
 function websocketConnect() {
     websocket = new Gdax.WebsocketClient(
@@ -117,106 +146,140 @@ function websocketConnect() {
             ]
         }
     );
-}
 
-websocket.on('message', data => {
-    if (data.type == 'ticker') {
-        //console.log(data);
-        currentPrice = parseFloat(data.price).toFixed(1);
-        if (!price) {
-            price = currentPrice;
-        }
-        if (lastPrice != currentPrice) {
-            var action = "";
-            var value = "";
-            if (log.length() == 2) {
+    websocket.on('message', data => {
+        if (data.type == 'ticker') {
+            //console.log(data);
+            currentPrice = parseFloat(data.price).toFixed(1);
+            if (!price) {
+                price = currentPrice;
+                for(var i = 0; i < logLength; i++) {
+                    log.push(price);
+                }
+            }
+            if (lastPrice != currentPrice) {
+                var action = "";
+                var value = "";
                 var values = log.values();
-                if (currentPrice > price + margin && (lastMode == "buy" || !lastMode) && (values[0] > values[1] && values[1] > currentPrice)) {
-                    action = "buy";
-                    value = parseFloat(data.best_bid) + 0.1;
-                }else if (currentPrice < price - margin && (lastMode == "sell" || !lastMode) && (values[0] < values[1] && values[1] < currentPrice)) {
-                    action = "sell";
-                    value = parseFloat(data.best_bid) - 0.1;
+                // Are we in a buy or sell mood
+                if (currentPrice > (price + margin) && (lastMode == "buy" || !lastMode)) {
+                    // Up
+                    mode = "sell";
+                }else if (currentPrice < (price - margin) && (lastMode == "sell" || !lastMode)) {
+                    // Down
+                    mode = "buy"
+                }else {
+                    mode = "";
+                }
+                if (mode) {
+                    console.log(mode);
+                }else{
+                    console.log("none");
+                }
+                if (mode == "buy" && currentPrice > lastPrice) {
+                    // we are in Buy mood and price has risen. See if risen for last 4 times + now
+                    if (values[0] < values[1] && values[1] < values[2] && values[2] < values[3] && values[3] < currentPrice) {
+                        action = "buy";
+                        value = parseFloat(data.best_bid) + 0.1;
+                    }
+                }else if (mode == "sell" && currentPrice < lastPrice) {
+                    // we are in Sell mood and price has fallen. See if fallen for last 4 times + now
+                    if (values[0] > values[1] && values[1] > values[2] && values[2] > values[3] && values[3] > currentPrice) {
+                        action = "sell";
+                        value = parseFloat(data.best_bid) - 0.1;
+                    }
                 }
                 if (action) {
                     doDeal(action, value);
                     price = value;
                     lastMode = action;
                 }
+                if (save) {
+                    con.query("INSERT INTO bot (price, mode, value) VALUES (?, ?, ?)", [currentPrice, action, value], function (err, result) {
+                        if (err) throw err;
+                    });
+                }
+                lastPrice = currentPrice;
+                if (mode) {
+                    // Only save to log if within sell or buy
+                    log.push(currentPrice);
+                }
             }
-            con.query("INSERT INTO bot (price, mode, value) VALUES (?, ?, ?)", [currentPrice, action, value], function (err, result) {
-                if (err) throw err;
-            });
-            lastPrice = currentPrice;
-            log.push(currentPrice);
         }
-    }
-});
-websocket.on('error', err => { /* handle error */ });
-websocket.on('close', () => { websocketConnect() });
+    });
+    websocket.on('error', err => { /* handle error */ });
+    websocket.on('close', () => {
+        console.log("Websocket closed");
+        setTimeout(websocketConnect, 500);
+        return;
+    });
+}
 
 function doDeal(action, value) {
     for (var key in users) {
         doUserDeal(users[key], action, value)
     };
+    con.query("UPDATE bot_last SET price = ?", [value], function (err, result) {
+        if (err) throw err;
+    });
 }
 
 function doUserDeal(user, action, value) {
     if (action == "buy") {
-        // Get Euro account
-        user.client.getAccount(user.euro_id).then(data => {
-            var user = users[data.profile_id];
-            var size = 0;
-            if (data.available > 1) {
-                if (amount) {
-                    size = amount / value;
-                }else{
-                    size = data.available / value
-                }
-                const params = {
-                    'side': action,
-                    'price': value, // USD
-                    'size': size, // LTC
-                    'product_id': product,
-                    'post_only': true,
-                    'time_in_force': 'IOC',
-                    'client_oid': uuidv1()
-                };
-                var a=1;
+        // Use Euro account
+        var size = 0;
+        if (user.euro.available > 1) {
+            if (amount) {
+                size = amount / value;
+            } else {
+                size = parseFloat(user.euro.available) / value
             }
-        })
-        .catch(error => {
-                var a=1;
-        });
+            const params = {
+                'side': action,
+                'price': value,
+                'size': size.toFixed(8),
+                'product_id': product,
+                'post_only': true,
+                'client_oid': uuidv1()
+            };
+            /*
+            user.client
+                .buy(params)
+                .then(data => {
+                    var a=1;
+            })
+            .catch(error => {
+                // handle the error
+            });
+            */
+        }
     }else{
-        // Get Currency account
-        user.client.getAccount(user.currency_id).then(data => {
-            var user = users[data.profile_id];
-            var size = 0;
-            if (data.available > 1) {
-                if (amount) {
-                    size = amount;
-                }else{
-                    size = data.available
-                }
-                const params = {
-                    'side': action,
-                    'price': value, // USD
-                    'size': size, // LTC
-                    'product_id': product,
-                    'post_only': true,
-                    'time_in_force': 'IOC'
-                };
-                var a=1;
+        // Use Currency account
+        var size = 0;
+        if (user.currency.available > 1) {
+            if (amount) {
+                size = amount;
+            }else{
+                size = parseFloat(user.currency.available);
             }
-        })
-        .catch(error => {
-                var a=1;
-        });
+            const params = {
+                'side': action,
+                'price': value,
+                'size': size.toFixed(8),
+                'product_id': product,
+                'post_only': true,
+                'client_oid': uuidv1()
+            };
+            /*
+            user.client
+                .sell(params)
+                .then(data => {
+                    var a=1;
+            })
+            .catch(error => {
+                // handle the error
+            });
+            */
+        }
     }
-
-
-
-
-
 }
